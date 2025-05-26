90deab6992b2b58232cbcb7a45b1855ba3fe2304
unknown <zda23m001@iitmz.ac.in>
2025-05-26 12:39:16 +0300

    Your commit message

--- src/train.py
+++ src/train.py
@@ -1,5 +1,8 @@
 from typing import Any, Dict, List, Optional, Tuple
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 import torch
 import hydra
 import lightning as L
